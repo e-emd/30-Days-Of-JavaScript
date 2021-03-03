ebf4d78aed6e28051b7cd35d3125cf33b39f956d
e-emd <dewin.emd@gmail.com>
2021-03-02 21:01:25 -0700

    day 4 excersise level 1

--- 04_Day_Conditionals/04_day_starter/scripts/main.js
+++ 04_Day_Conditionals/04_day_starter/scripts/main.js
@@ -1,3 +1,15 @@
 // this is your main.js script
+alert('Open the browser console whenever you work on JavaScript');
 
-alert('Open the browser console whenever you work on JavaScript')
+const drivingAge = prompt('Enter your age');
+
+switch (true) {
+  case drivingAge >= 18:
+    console.log(`You are old enough to drive`);
+    break;
+  case drivingAge < 18:
+    console.log(`You need to be older than 18 to drive`);
+    break;
+  default:
+    console.log(`Enter value was not a number`);
+}
