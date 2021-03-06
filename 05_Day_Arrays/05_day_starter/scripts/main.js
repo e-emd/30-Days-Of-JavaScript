1c24ad2c66a7abf994bbb3fa585f5291be0cc5a8
e-emd <dewin.emd@gmail.com>
2021-03-05 21:27:43 -0700

    day 5 exercise level 1

--- 05_Day_Arrays/05_day_starter/scripts/main.js
+++ 05_Day_Arrays/05_day_starter/scripts/main.js
@@ -1,3 +1,60 @@
-console.log(countries)
-alert('Open the browser console whenever you work on JavaScript')
-alert('Open the console and check if the countries has been loaded')
+console.log(countries);
+alert('Open the browser console whenever you work on JavaScript');
+alert('Open the console and check if the countries has been loaded');
+
+// Exercise: Level 1
+let arr = [];
+const webTechs = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'React',
+  'Redux',
+  'Node',
+  'MongoDB',
+];
+const middleArr = webTechs[Math.floor(webTechs.length / 2)];
+const lastItem = webTechs[webTechs.length - 1];
+let mixedDataTypes = [
+  [1, 2, 3],
+  'hello',
+  {
+    firstName: 'edwin',
+    lastName: 'Marquez',
+  },
+  9,
+  true,
+];
+const itCompanies = [
+  'Facebook',
+  'Google',
+  'Microsoft',
+  'Apple',
+  'IBM',
+  'Oracle',
+  'Amazon',
+];
+const middleCompany = itCompanies[Math.floor(itCompanies.length / 2)];
+const lastCompany = itCompanies[itCompanies.length - 1];
+const uppercaseCompany = itCompanies.map((x) => {
+  return x.toUpperCase();
+});
+const sentenceArr = itCompanies.slice(0, itCompanies.length - 1).join(', ');
+const itSearch = prompt('Search for an it company');
+const arrContains = itCompanies.indexOf(itSearch) > -1;
+if (arrContains) {
+  console.log(`${itSearch} is found`);
+} else {
+  console.log(`${itSearch} was not found`);
+}
+console.log(webTechs.length);
+console.log(webTechs[0]);
+console.log(middleArr);
+console.log(lastItem);
+console.log(mixedDataTypes.length);
+console.log(itCompanies);
+console.log(itCompanies.length);
+console.log(middleCompany);
+console.log(lastCompany);
+console.log(uppercaseCompany);
+console.log(`${sentenceArr} and ${lastCompany} are big IT companies`);
